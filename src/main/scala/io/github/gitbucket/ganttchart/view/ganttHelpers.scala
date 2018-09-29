0e93a052779c37f2172b4dd5f7b0353d097e30ad
kasancode <kasancode@gmail.com>
2018-09-29 19:17:33 +0900

    escape HTML

--- src/main/scala/io/github/gitbucket/ganttchart/view/ganttHelpers.scala
+++ src/main/scala/io/github/gitbucket/ganttchart/view/ganttHelpers.scala
@@ -1,23 +1,15 @@
 package io.github.gitbucket.ganttchart.view
 
-import java.text.SimpleDateFormat
-import java.util.{Date, Locale, TimeZone}
-
-import com.nimbusds.jose.util.JSONObjectUtils
 import gitbucket.core.controller.Context
 import gitbucket.core.model.{Comment, CommitState, Issue, IssueComment}
-import gitbucket.core.plugin.{PluginRegistry, RenderRequest}
-import gitbucket.core.service.RepositoryService.RepositoryInfo
 import gitbucket.core.service.{RepositoryService, RequestCache}
-import gitbucket.core.util.{FileUtil, JGitUtil, StringUtil}
-import gitbucket.core.view.helpers.createIssueLink
 import gitbucket.core.view.{AvatarImageProvider, LinkConverter, helpers}
-import play.twirl.api.{Html, HtmlFormat}
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
+import gitbucket.core.util.StringUtil
 
 
 object ganttHelpers extends AvatarImageProvider with LinkConverter with RequestCache {
+  def escapeHtml(value: Option[String]): String = escapeHtml(value.getOrElse(""))
+  def escapeHtml(value: String): String = StringUtil.escapeHtml(value)
 
   def createMilestoneUrl(repository: RepositoryService.RepositoryInfo, milestone: String)(
     implicit context: Context
@@ -25,7 +17,7 @@ object ganttHelpers extends AvatarImageProvider with LinkConverter with RequestC
     val userName = repository.repository.userName
     val repositoryName = repository.repository.repositoryName
 
-    s"""${context.path}/${userName}/${repositoryName}/issues?milestone=${URLEncoder.encode(milestone, StandardCharsets.UTF_8.toString)}&state=open"""
+    s"""${context.path}/${userName}/${repositoryName}/issues?milestone=${helpers.urlEncode(milestone)}&state=open"""
   }
 
   def createIssueUrl(repository: RepositoryService.RepositoryInfo, issue: Issue)(
