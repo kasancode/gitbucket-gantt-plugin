45d419f96f4bf9179200a4ff3e883d625bb02774
kasancode <kasancode@gmail.com>
2018-10-05 00:45:38 +0900

    refactoring

--- src/main/resources/plugins/gantt/assets/plugin-gantt.js
+++ src/main/resources/plugins/gantt/assets/plugin-gantt.js
@@ -1,36 +1,33 @@
 
-$(document).ready(function(){
-    var shownTooltip = false;
-
-    function getTip(){
-        if($("#gantt-tooltip").length > 0)
+$(document).ready(function () {
+    function getTip() {
+        if ($("#gantt-tooltip").length > 0)
             return $("#gantt-tooltip");
 
         return $("<div />")
-           .attr("id","gantt-tooltip")
-           .appendTo("body");
+            .attr("id", "gantt-tooltip")
+            .appendTo("body");
     }
 
     $("circle.gantt-point").on(
         {
-            "mouseenter":function(ev){
-
+            "mouseenter": function (ev) {
                 var $point = $(ev.currentTarget);
                 var pos = $point.offset();
                 var $title = $point.children("title");
-                var data = $point.attr("data-gantt-tooltip") || function(){
-                    $point.attr("data-gantt-tooltip",$title.html());
+                var data = $point.attr("data-gantt-tooltip") || function () {
+                    $point.attr("data-gantt-tooltip", $title.html());
                     $title.html("");
                     return $point.attr("data-gantt-tooltip");
                 }();
 
-                if(!data)
+                if (!data)
                     return;
 
                 var $tip = getTip().html(data);
 
                 var rect = $point[0].getBoundingClientRect();
-                if(rect.width == null){
+                if (rect.width == null) {
                     rect.width = rect.right - rect.left;
                     rect.height = rect.bottom - rect.top;
                 }
@@ -38,17 +35,16 @@ $(document).ready(function(){
                 pos.top -= $tip.height() + rect.height / 2 + 30;
                 pos.left -= $tip.width() / 2 - rect.width / 2 + 10;
 
-                shownTooltip = true;
                 $tip.css(pos).fadeIn(10);
             }
             ,
-            "mouseleave":function(ev){
-                if(!shownTooltip)
-                    return;
-
+            "mouseleave": function (ev) {
                 var $tip = getTip();
 
-                $tip.fadeOut(10, function(){
+                if ($tip.css("display") === "none")
+                    return;
+
+                $tip.fadeOut(10, function () {
                     shownTooltip = false;
                 });
             }
